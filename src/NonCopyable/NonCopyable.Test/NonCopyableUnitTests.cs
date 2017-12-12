e78333a75156001da802536f085ab607e840c343
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-12-12 15:14:01 +0900

    Add projects

---
+++ src/NonCopyable/NonCopyable.Test/NonCopyableUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using TestHelper;
+using Xunit;
+using NonCopyable;
+
+namespace NonCopyable.Test
+{
+    public class NonCopyableUnitTests : ConventionCodeFixVerifier
+    {
+        [Fact]
+        public void EmptySource() => VerifyCSharpByConvention();
+
+        [Fact]
+        public void LowercaseLetters() => VerifyCSharpByConvention();
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider() => new NonCopyableCodeFixProvider();
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new NonCopyableAnalyzer();
+    }
+}
