82d2e099d1f1ec98599d742faffe2765778fd5ad
Victor Milovanov <lostfreeman@gmail.com>
2020-12-02 11:13:43 -0800

    refactored Diagnostic.Create out for easier debugging

--- src/NonCopyable/NonCopyable/NonCopyableAnalyzer.cs
+++ src/NonCopyable/NonCopyable/NonCopyableAnalyzer.cs
@@ -86,7 +86,7 @@ namespace NonCopyable
                         return; 
                     }
 
-                    oc.ReportDiagnostic(Diagnostic.Create(ConversionRule, v.Syntax.GetLocation(), t.Name));
+                    oc.ReportDiagnostic(Error(v.Syntax, ConversionRule, t.Name));
                 }, OperationKind.Conversion);
 
                 csc.RegisterOperationAction(oc =>
@@ -106,7 +106,7 @@ namespace NonCopyable
                     var op = (IDeclarationPatternOperation)oc.Operation;
                     var t = ((ILocalSymbol)op.DeclaredSymbol).Type;
                     if (!t.IsNonCopyable()) return;
-                    oc.ReportDiagnostic(Diagnostic.Create(PatternRule, op.Syntax.GetLocation(), t.Name));
+                    oc.ReportDiagnostic(Error(op.Syntax, PatternRule, t.Name));
                 }, OperationKind.DeclarationPattern);
 
                 csc.RegisterOperationAction(oc =>
@@ -146,7 +146,7 @@ namespace NonCopyable
                     foreach(var arg in op.Arguments) {
                         if (!arg.Type.IsNonCopyable()) continue;
 
-                        oc.ReportDiagnostic(Diagnostic.Create(GenericConstraintRule, arg.Syntax.GetLocation()));
+                        oc.ReportDiagnostic(Error(arg.Syntax, GenericConstraintRule));
                     }
 
                 }, OperationKind.DynamicInvocation);
@@ -157,7 +157,7 @@ namespace NonCopyable
                     var op = (IMemberReferenceOperation)oc.Operation;
                     if (op.Instance == null) return;
                     if (!op.Instance.Type.IsNonCopyable()) return;
-                    oc.ReportDiagnostic(Diagnostic.Create(DelegateRule, op.Instance.Syntax.GetLocation(), op.Instance.Type.Name));
+                    oc.ReportDiagnostic(Error(op.Instance.Syntax, DelegateRule, op.Instance.Type.Name));
                 }, OperationKind.MethodReference);
 
                 csc.RegisterSymbolAction(sac =>
@@ -167,7 +167,7 @@ namespace NonCopyable
                     if (!f.Type.IsNonCopyable()) return;
                     if (f.ContainingType.IsReferenceType) return;
                     if (f.ContainingType.IsNonCopyable()) return;
-                    sac.ReportDiagnostic(Diagnostic.Create(FieldDeclarationRule, f.DeclaringSyntaxReferences[0].GetSyntax().GetLocation(), f.Type.Name));
+                    sac.ReportDiagnostic(Error(f.DeclaringSyntaxReferences[0].GetSyntax(), FieldDeclarationRule, f.Type.Name));
                 }, SymbolKind.Field);
             });
 
@@ -191,7 +191,7 @@ namespace NonCopyable
                     var a = arguments[i];
 
                     if (a.IsNonCopyable() && !p.IsNonCopyable())
-                        oc.ReportDiagnostic(Diagnostic.Create(rule, op.Syntax.GetLocation(), a.Name));
+                        oc.ReportDiagnostic(Error(op.Syntax, rule, a.Name));
                 }
             }
         }
@@ -205,9 +205,14 @@ namespace NonCopyable
 
             if (IsInstanceReadonly(instance))
             {
-                oc.ReportDiagnostic(Diagnostic.Create(rule, instance.Syntax.GetLocation(), t.Name));
+                oc.ReportDiagnostic(Error(instance.Syntax, rule, t.Name));
             }
         }
+
+        private static Diagnostic Error(SyntaxNode at, DiagnosticDescriptor rule, string name = null)
+            => name is null
+                ? Diagnostic.Create(rule, at.GetLocation())
+                : Diagnostic.Create(rule, at.GetLocation(), name);
 
         private static bool IsInstanceReadonly(IOperation instance)
         {
@@ -245,7 +250,7 @@ namespace NonCopyable
             var t = v.Type;
             if (!t.IsNonCopyable()) return;
             if (v.CanCopy()) return;
-            oc.ReportDiagnostic(Diagnostic.Create(rule, v.Syntax.GetLocation(), t.Name));
+            oc.ReportDiagnostic(Error(v.Syntax, rule, t.Name));
         }
     }
 }
